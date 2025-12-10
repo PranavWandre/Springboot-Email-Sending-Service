a6e9d43c13e4517c7e0d1fb4389d612c0ff27618
Pranav Wandre <pranavwandre98@gmail.com>
2025-12-10 20:30:56 +0530

    first commit

---
+++ src/main/java/com/email/demo/EmailSendingApi041225Application.java
@@ -0,0 +1,14 @@
+package com.email.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSendingApi041225Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSendingApi041225Application.class, args);
+        System.out.println("STARTED-----------------------------------");
+    }
+
+}
---
+++ src/main/java/com/email/demo/controller/EmailController.java
@@ -0,0 +1,29 @@
+package com.email.demo.controller;
+
+import com.email.demo.dto.EmailRequest;
+import com.email.demo.service.EmailService;
+
+import jakarta.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/send")
+    public String sendEmail(@RequestBody EmailRequest request) {
+        return emailService.sendEmailWithPdf(
+                request.getTo(),
+                request.getSubject(),
+                request.getBody(),
+                request.getFileName()
+        );
+    }
+
+}
